c9861ec3323c9f96b9ae4c11ab02d6f2511f3774
pkallas <pkallas29@gmail.com>
2018-02-27 10:51:33 -0800

    Started creating components for new Yelp-Tinder app

--- src/client/components/home_page/index.jsx
+++ src/client/components/home_page/index.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Restaurant from '../restaurant';
+import RestaurantSelectButtons from '../restaurant_select_buttons';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <div className='text'>
-        Thanks for using my starting template!
+      <div>
+        <Restaurant name='Burma Superstar' rating='5 Stars' location='Alameda' cost='$$'  />
+        <RestaurantSelectButtons />
       </div>
     );
-  };
+  }
 }
---
+++ src/client/components/restaurant_select_buttons/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class RestaurantSelectButtons extends Component {
+  render() {
+    return (
+      <div className='text'>This is the RestaurantSelectButtons component.</div>
+    );
+  }
+}
